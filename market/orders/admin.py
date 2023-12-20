25d207de3812b1bb9a0817f0bff3f79f5072ca66
Evgeny Korolev <ytrokinterteiment@gmail.com>
2023-12-20 15:37:25 +0100

    Modified admin panel

--- market/orders/admin.py
+++ market/orders/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
