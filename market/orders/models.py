cea4341bb5407456f00d692367ddcfdcca59d428
Evgeny Korolev <ytrokinterteiment@gmail.com>
2023-12-20 15:36:52 +0100

    Modified Order, OrderItem models and created new migrations

--- market/orders/models.py
+++ market/orders/models.py
@@ -1,15 +1,12 @@
 from django.db import models
-
 from products.models import Product
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 class Order(models.Model):
-    phone_number = PhoneNumberField()
-    address = models.TextField()
+    phone_number = PhoneNumberField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
     landmark = models.CharField(max_length=100, blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
-    products = models.ManyToManyField(Product, through='OrderItem')
     total_price = models.FloatField(null=True, blank=True)
     delivery_price = models.FloatField(null=True, blank=True)
     order_date = models.DateTimeField(auto_now_add=True)
@@ -18,9 +15,9 @@ class Order(models.Model):
         return f"Order #{self.id} - Total Price: {self.total_price}"
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"Order #{self.order.id} - {self.product.title} - Quantity: {self.quantity}"
+        return f"Order #{self.order_id.id} - {self.product_id.title} - Quantity: {self.quantity}"
