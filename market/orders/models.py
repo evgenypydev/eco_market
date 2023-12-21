6f851b8c9782e1cf114f1dd87d13945ab19f3eb7
Evgeny Korolev <ytrokinterteiment@gmail.com>
2023-12-21 12:57:16 +0100

    Created new serializers for Order, OrderItem models.

--- market/orders/models.py
+++ market/orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from products.models import Product
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class Order(models.Model):
     phone_number = PhoneNumberField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
@@ -13,6 +14,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - Total Price: {self.total_price}"
+
 
 class OrderItem(models.Model):
     order_id = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
--- market/orders/serializers.py
+++ market/orders/serializers.py
@@ -1,18 +1,48 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
-from products.serializers import ProductCRUDSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = ProductCRUDSerializer()
 
     class Meta:
         model = OrderItem
-        fields = ["id", "order", "product", "quantity"]
+        fields = ['product_id', 'quantity']
 
-class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
+
+class OrderCreateUpdateSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, required=True)
 
     class Meta:
         model = Order
-        fields = ["id", "phone_number", "address", "landmark", "comment", "products", "total_price", "delivery_price", "order_price"]
+        fields = ["id", "order_items", "phone_number", "address", "landmark", "comment", "total_price",
+                  "delivery_price", "order_date"]
+
+    def create(self, validated_data):
+        order_items_data = validated_data.pop('order_items')
+        order = Order.objects.create(**validated_data)
+        for order_item_data in order_items_data:
+            OrderItem.objects.create(order_id=order, **order_item_data)
+        return order
+
+    def update(self, instance, validated_data):
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.address = validated_data.get('address', instance.address)
+        instance.landmark = validated_data.get('landmark', instance.landmark)
+        instance.comment = validated_data.get('comment', instance.comment)
+        instance.delivery_price = validated_data.get('delivery_price', instance.delivery_price)
+        instance.total_price = (sum(item.product_id.price * item.quantity for item in instance.order_items.all() if
+                                item.product_id.price is not None) +
+                                (instance.delivery_price if instance.delivery_price is not None else 0))
+
+        instance.save()
+
+        return instance
+
+
+class OrderListRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ["id", "order_items", "phone_number", "address", "landmark", "comment", "total_price",
+                  "delivery_price", "order_date"]
+
