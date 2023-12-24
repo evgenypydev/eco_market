1a5975f10b43ceef2d3dc80270828ba0a6c4f6bb
Evgeny Korolev <ytrokinterteiment@gmail.com>
2023-12-24 14:36:59 +0100

    Updated serializers(added new image field)

--- market/products/serializers.py
+++ market/products/serializers.py
@@ -2,15 +2,16 @@ from rest_framework.relations import StringRelatedField
 from rest_framework.serializers import ModelSerializer
 from .models import Product, Category
 
+
 class ProductCRUDSerializer(ModelSerializer):
-    category = StringRelatedField()
 
     class Meta:
         model = Product
-        fields = ["id", "title", "capacity", "description", "price", "category"]
+        fields = ["id", "title", "image", "capacity", "description", "price", "category"]
+
 
 class CategoryCRUDSerializer(ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ["id", "title"]
+        fields = ["id", "title", "image"]
