06db2aa41e13e82f3dd9574adce9e37f18f293a4
Evgeny Korolev <ytrokinterteiment@gmail.com>
2023-12-26 16:22:37 +0100

    Added Swagger

--- market/market/urls.py
+++ market/market/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Neobis-Eco-Market",
+        default_version='v1',
+        description="Eco Market REST API",
+    ),
+    public=True,
+)
+
+swagger_info = schema_view.with_ui('swagger', cache_timeout=0)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', swagger_info, name='swagger'),
     path("", include("products.urls")),
     path("", include("orders.urls")),
 ]
